8a7237d525c93d0197fcd234459f519b33e69492
agustin-gonzatto <gonzattoagustin1@gmail.com>
2022-11-29 12:38:29 -0300

    Responsive,email

--- js/terminal.js
+++ js/terminal.js
@@ -1,12 +1,5 @@
-var words = ["Hola! soy Agustín Gonzatto", "Desarrollador Front-end", "Estudiante de Ingeniería Eléctrica", " Trabajemos juntos!"],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 70;
+var words = ["Hola! soy Agustín Gonzatto", "Desarrollador Front-end", "Estudiante de Ingeniería Eléctrica"]
+let i = 0;
 let j = 0;
 let cont = 0;
 
